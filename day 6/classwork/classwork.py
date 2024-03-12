c227ac7358cd403b9d019025c0f521cc144c3306
Nikusha1 <nikalasoselia@gmail.com>
2024-03-12 14:11:39 +0400

    second commit

---
+++ day 6/classwork/classwork.py
@@ -0,0 +1,24 @@
+#num1 = input("please enter first number: ")
+#num2 = input("please enter second number: ")
+
+#print(num1, num2) 
+
+
+
+print (45 < 150) 
+print (142 > 211)
+print (20 < 2)
+print (30 > 15)
+print (90 < 12)
+print (12.5 > 11)
+print (40.5 < 50.1)
+print (110 > 65)
+print (70.8 > 70)
+print (80 < 79)
+print (200 > 0.5)
+print (87 > 89)
+print (10 < 10.1)
+print (76 < 99)
+print (19 > 166)
+print (1000 > 200000)
+print (155000 < 5500)
