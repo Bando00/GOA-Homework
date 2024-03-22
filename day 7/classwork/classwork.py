61fe9235539768a0bb9b368a8058455b89653fc6
Nikusha1 <nikalasoselia@gmail.com>
2024-03-22 10:50:52 +0400

    second commit

---
+++ day 7/classwork/classwork.py
@@ -0,0 +1,34 @@
+#print(False or False) = false
+#print(False and True) = false false 
+
+
+
+
+
+#name = "nika"
+#name = "mate"
+#name = "giorgi"
+
+
+
+# print(name)
+# print(name)
+# print(name)
+
+
+age = 15
+Age = 5
+
+#case sensitive language - ქეის სენსიტიური ენა - დიდ და პატარა ასოებს აქვს ძალიან დიდი მნიშვნელობა
+print(age) #lowercase - პატარა ასოები
+print(Age) #uppercase - დიდი ასოები
+
+#print(True or False) = True
+
+
+num1 = 9
+print(type(num1))
+
+
+
+print("active or registered")
