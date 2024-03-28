91f0a7bf1f039e01f0caac5969e2ae06101b2f9b
Nikusha1 <nikalasoselia@gmail.com>
2024-03-28 10:23:06 +0400

    second commit

---
+++ day 7/homework/homework.py
@@ -0,0 +1,26 @@
+name = input("Enter Your Name: ")
+num1 = int(input("Enter Integer Number: "))
+float_num = float(input("Enter Float Number: "))
+bool_value = bool(input("Enter True/False: "))
+
+print(name)
+print(num1)
+print(float_num)
+print(bool_value)
+
+
+
+
+
+
+
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
