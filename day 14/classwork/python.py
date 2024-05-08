76cbbbe3192679693feebd15852361463451a247
Nikusha1 <nikalasoselia@gmail.com>
2024-05-08 14:36:12 +0400

    second commit

---
+++ day 14/classwork/python.py
@@ -0,0 +1,53 @@
+#print('Hello world')
+
+
+#print(10 > 5)
+
+#name = input("Enter ur name: ")
+
+#print('heelo' + name)
+
+#age =int(input("enter your age: "))
+
+#თუ ასაკი მეტი იქნება 18 შესრულდეს რაღაც მოქმედება
+
+#age = input("enter your age: ")
+
+#if age > 18:
+   # print("სრულწლოვანი ხარ")
+#else:
+ #   print("არასრულწლოვანი ხარ")    
+
+
+
+#score = int(input("enter your score: ")) 
+
+#if score > 90:
+   # print("you did a good job!")
+#elif score > 70:
+   # print("get better")
+#elif score == 50:
+    #print("u got half score")
+#else:    
+   # print("you got a bad score, try harder next time!")    
+
+
+
+#elif = else + if
+
+
+
+
+
+
+
+group = int(input("enter your group name: "))
+
+if group == 19:
+    print("ჯგუფის მენტორი არის გაბრიელი")
+elif group == 20:
+    print("ჯგუფის მენტორი არის ლუკა")
+elif group == 21:
+    print("ჯგუფის მენტორი არის დავითი")
+else:
+    print("სხვა ჯგუფების მენტორია ნიკა")
---
+++ day 14/homework/homework.py
@@ -0,0 +1,62 @@
+number = int(input("Enter random number from 1-30: "))
+#odd = კენტი, even = ლუწი
+if number == 1:
+    print("this number is Odd")
+elif number == 2:
+    print("this number is even")
+elif number == 3:
+    print("this number is odd")
+elif number == 4:
+    print("this number is even")
+elif number == 5:
+    print("this number is odd")
+elif number == 6:
+    print("this number is even")
+elif number == 7:
+    print("this number is odd")
+elif number == 8:
+    print("this number is even")
+elif number == 9:
+    print("this number is odd")
+elif number == 10:
+    print("this number is even")
+elif number == 11:
+    print("this number is odd")
+elif number == 12:
+    print("this number is even")
+elif number == 13:
+    print("this number is odd")
+elif number == 14:
+    print("this number is even")
+elif number == 15:
+    print("this number is odd")
+elif number == 16:
+    print("this number is even")
+elif number == 17:
+    print("this number is odd")
+elif number == 18:
+    print("this number is even")
+elif number == 19:
+    print("this number is odd")
+elif  number == 20:
+    print("this number is even")
+elif number == 21:
+    print("this number is odd")
+elif number == 22:
+    print("this number is even")
+elif number == 23:
+    print("this number is odd")
+elif number == 24:
+    print("this number is even")
+elif number == 25:
+    print("this number is odd")
+elif number == 26:
+    print("this number is even")
+elif number == 27:
+    print("this number is odd")
+elif number == 28:
+    print("this number is even") 
+elif number == 29:
+    print("this number is odd")
+elif number == 30:
+    print("this number is even")                
