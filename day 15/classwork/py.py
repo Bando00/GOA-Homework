290eda051ea617e693ef44558f40c8ee195052b7
Nikusha1 <nikalasoselia@gmail.com>
2024-05-19 15:03:03 +0400

    second commit

---
+++ day 15/classwork/py.py
@@ -0,0 +1,10 @@
+#მომხმარებელს უნდა შემოატანინოთ რიცხვი და შემდეგ უნდა დაბეჭდოთ არის თუ არა ეს რიცხვი ხუთის ჯერადი
+
+number = int(input("Enter your number: "))
+
+if number % 5 == 0:
+    print("This number is a multiple of five")
+else:
+    print("This number is not a multiple of five")
+
+
