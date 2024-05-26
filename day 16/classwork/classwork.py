1de4ecf8a63d11c7eeb6b3232d920b68a9981beb
Nikusha1 <nikalasoselia@gmail.com>
2024-05-26 16:28:10 +0400

    second commit

---
+++ day 16/classwork/classwork.py
@@ -0,0 +1,33 @@
+#for i in range(50):   
+  # if i % 2 ==1:
+   #   print(i)
+
+
+
+# range ( start , end , step )
+for i in range(5):
+    print('Goa is Best' , i)
+    for j in range(5):
+        print("group 19" , j)
+        
+        
+
+
+
+
+#iteration = ბრუნვა , გამეორება
+
+
+
+
+
+
+#for i in range(30, 120, 10):
+  #  print(i)
+
+    
+    
+ 
+    
+
+    
---
+++ day 16/classwork/py.py
@@ -0,0 +1,15 @@
+for letter in range(125):   
+    print(letter)
+#აქ ითვლის 125 ამდე
+  
+
+
+#for in range(125):
+ #   print("hello world")  
+ # აქ გამოაქ hello world 125_ჯერ
+
+
+# i - საინტერაციო ცვლადი
+
+#varieble = ცვლადი
+
