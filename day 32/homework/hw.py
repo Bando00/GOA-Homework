59360a6ba8dd4b14aa69cd8062d30fa82676ae78
Nikusha1 <nikalasoselia@gmail.com>
2024-09-15 15:34:01 +0400

    second commit

---
+++ day 32/homework/hw.py
@@ -0,0 +1,16 @@
+#codewars
+
+# Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+def repeat_str(repeat, string):
+    return repeat * string
+
+
+
+
+# Write a function `greet` that returns "hello world!"
+
+def greet():
+    return "hello world!"
+
+
