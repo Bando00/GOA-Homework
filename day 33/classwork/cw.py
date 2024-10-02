e3647efa1242748b2ae63f30aaf5d5551880abc1
Nikusha1 <nikalasoselia@gmail.com>
2024-10-02 17:30:25 +0400

    second commit

---
+++ day 33/classwork/cw.py
@@ -0,0 +1,12 @@
+# გადმოგეცემა სია, ამ სიიდან შევკრიბოთ ყველა დადებითი რიცხვი
+
+
+def positive_numbers_sum(list):
+
+    for i in range(len(list)):
+        print(list[i])
+
+
+
+list1 = [1, 2, 3, 4, 5, 6, -3, 4, -10, -50]
+print(positive_numbers_sum(list))
