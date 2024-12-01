89803a1fc74370d82bc0e0b6e4342a7f37cfe63e
Nikusha1 <nikalasoselia@gmail.com>
2024-12-01 16:35:04 +0400

    second commit

---
+++ day 43/classwork/cw.py
@@ -0,0 +1,11 @@
+def positive_sum(arr):
+    return sum(x for x in arr if x > 0)
+
+
+
+
+#def psitive_sum(arr):
+ #   sum = 0
+ #   for i in arr:
+
+
---
+++ day 43/homework/p.py
@@ -0,0 +1,19 @@
+# Code as fast as you can! You need to double the integer and return it.
+def double_integer(i):
+    return i * 2
+
+
+
+
+
+
+# Make a simple function called greet that returns the most-famous "hello world!".
+def greet():
+    return "hello world!"
+
+
+# Complete the solution so that it reverses the string passed into it.
+def solution(string):
+    return string[::-1]
+
+
