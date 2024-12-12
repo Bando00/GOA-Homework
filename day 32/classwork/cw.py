0232ab4084bb2843e25b813d81853ac2d9537183
Nikusha1 <nikalasoselia@gmail.com>
2024-12-12 10:56:09 +0400

    second commit:

---
+++ day 32/classwork/cw.py
@@ -0,0 +1,17 @@
+# 1) def even_or_odd(number):
+#     if number % 2 == 0
+#     return "Even"
+#  else:
+#     return "Odd" 
+
+
+
+# 2) def multiply(a, b)
+#     return a * b
+
+
+# 3) def make_negative(number)
+#     if number > 0:
+#        return number * -1
+#   else:
+#        return number     
