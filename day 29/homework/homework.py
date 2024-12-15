bcea0fefa63da7c88df385bf19c63d126ff02de7
Nikusha1 <nikalasoselia@gmail.com>
2024-12-15 21:07:57 +0400

    second commit

---
+++ day 29/homework/homework.py
@@ -0,0 +1,18 @@
+# 1)
+# for i in range(0,20):
+
+# 2)
+# num=int(input("enter your number:"))
+# num=int(input("enter your number:"))
+# for i in range(num1,num2)
+#    print(i)
+
+
+# 3)
+# for i in range(50,101)
+#    print (i)
+
+
+# 4)
+# for i in range(100,49,-1):
+#     print(i)
