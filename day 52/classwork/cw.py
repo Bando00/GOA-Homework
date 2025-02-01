a06dbb6fa12224115c50daf9dd5819c5bce3777b
Nikusha1 <nikalasoselia@gmail.com>
2025-02-01 19:36:13 +0400

    second commkit

---
+++ day 52/classwork/cw.py
@@ -0,0 +1,14 @@
+def even_or_odd (number):
+    if number % 2 == 0:
+        return "even"
+    else:
+        return "odd"
+    
+
+
+def number_to_string(num):
+    return str(num)
+
+
+def solution(string):
+    pass solutio     
