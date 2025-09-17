e4c0335ba3017e612e59a6a1e7e6fa4c15d51f64
Nikusha1 <nikalasoselia@gmail.com>
2025-09-17 22:14:22 +0400

    second commit

---
+++ day 81/homework/hw.py
@@ -0,0 +1,6 @@
+def double_integer(i):
+    return i * 2
+
+
+def make_negative(number):
+    return -abs(number)
---
+++ day 82/classwork/cw.java
@@ -0,0 +1,7 @@
+var age = 20;        
+let city = "თბილისი"; // 
+const country = "საქართველო"; 
+
+console.log(age);     
+console.log(city);    
+console.log(country);
